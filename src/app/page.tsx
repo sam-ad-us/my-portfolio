728cb773dc4e1d5fc24930fa965716978c3ebb2a
ABDUS SAMAD <abduspnhs2005@gmail.com>
2025-09-13 08:44:02 +0000

    remove this (_for element div_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import ContactSection from '@/components/contact-section';
 import HeroSection from '@/components/hero-section';
-import ProjectsSection from '@/components/projects-section';
 import SkillsSection from '@/components/skills-section';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center overflow-x-hidden">
       <HeroSection />
-      <ProjectsSection />
       <SkillsSection />
       <ContactSection />
     </div>
