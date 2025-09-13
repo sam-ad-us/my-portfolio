9858949b8cd3a18c21ae96424b40222f5d3c0d45
ABDUS SAMAD <abduspnhs2005@gmail.com>
2025-09-13 09:19:42 +0000

    I want to add more Projects, . For this, add another page through this b

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import ProjectsSection from '@/components/projects-section';
+
+export default function ProjectsPage() {
+  return (
+    <div className="flex flex-col items-center">
+      <ProjectsSection />
+    </div>
+  );
+}
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -9,7 +9,7 @@ import * as React from 'react';
 
 const mainNav = [
   { name: 'About', href: '/about' },
-  { name: 'Projects', href: '#projects' },
+  { name: 'Projects', href: '/projects' },
   { name: 'Skills', href: '/skills' },
   { name: 'Contact', href: '#contact' },
   { name: 'CV Enhancer', href: '/cv-enhancer' },
