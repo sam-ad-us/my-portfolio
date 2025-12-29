ed87876439cff84ac9ec125565091f162d4fefcb
ABDUS SAMAD <abduspnhs2005@gmail.com>
2025-12-29 11:14:51 +0000

    also add these in env
    NEXT_PUBLIC_FIREBASE_API_KEY=
    NEXT_PUBLIC_FIREBAS

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  "projectId": "studio-1619180194-ebaaf",
-  "appId": "1:511615589972:web:3cdd5a9964a81ab6d417c1",
-  "storageBucket": "studio-1619180194-ebaaf.appspot.com",
-  "apiKey": "AIzaSyCz8At8PGJ5mVAD4C2cwygLhCefs_i8vLg",
-  "authDomain": "studio-1619180194-ebaaf.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "511615589972"
+  apiKey: "AIzaSyCz8At8PGJ5mVAD4C2cwygLhCefs_i8vLg",
+  authDomain: "studio-1619180194-ebaaf.firebaseapp.com",
+  projectId: "studio-1619180194-ebaaf",
+  storageBucket: "studio-1619180194-ebaaf.appspot.com",
+  messagingSenderId: "511615589972",
+  appId: "1:511615589972:web:3cdd5a9964a81ab6d417c1",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
