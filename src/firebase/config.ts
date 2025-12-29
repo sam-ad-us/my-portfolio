0aca67df4f4ece4c18a189ed8714595bf95b6be9
ABDUS SAMAD <abduspnhs2005@gmail.com>
2025-12-29 07:20:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1619180194-ebaaf",
+  "appId": "1:511615589972:web:3cdd5a9964a81ab6d417c1",
+  "apiKey": "AIzaSyCz8At8PGJ5mVAD4C2cwygLhCefs_i8vLg",
+  "authDomain": "studio-1619180194-ebaaf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "511615589972"
+};
